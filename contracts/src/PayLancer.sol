87952c9e5fde3c88aecfddd1cb89ec200aa8b4dc
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-13 23:37:30 +0100

    feat: add structs and events

--- contracts/src/PayLancer.sol
+++ contracts/src/PayLancer.sol
@@ -2,5 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract PayLancer {
-    
+    struct Invoice {
+        uint256 id;
+        address creator;
+        address client;
+        uint256 amount;
+        address token;
+        bool isPaid;
+    }
+
+    mapping(uint256 => Invoice) public invoices;
+    uint256 public nextInvoiceId;
+
+    event InvoiceCreated(
+        uint256 indexed id,
+        address indexed creator,
+        address indexed client,
+        uint256 amount,
+        address token
+    );
+    event InvoicePaid(uint256 indexed id, address indexed payer);
 }
